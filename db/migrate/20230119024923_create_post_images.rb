88e6de3b1cc441e25b2e076e92f31ba79de3290c
EC2 Default User <ec2-user@ip-172-31-35-105.ap-northeast-1.compute.internal>
2023-01-19 04:18:12 +0000

    [add] 投稿機能3

--- db/migrate/20230119024923_create_post_images.rb
+++ db/migrate/20230119024923_create_post_images.rb
@@ -1,11 +1,16 @@
 class CreatePostImages < ActiveRecord::Migration[6.1]
   def change
     create_table :post_images do |t|
+
+      t.timestamps
       
       t.string :shop_name
       t.text :caption
       t.integer :user_id
-      
+    end
+    
+    create_table :PostImage, :id => false do |t|
+      t.integer :id, :primary_key => true
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_18_094738) do
+ActiveRecord::Schema.define(version: 2023_01_19_024923) do
+
+  create_table "PostImage", force: :cascade do |t|
+  end
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -41,6 +44,8 @@ ActiveRecord::Schema.define(version: 2023_01_18_094738) do
   end
 
   create_table "post_images", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.string "shop_name"
     t.text "caption"
     t.integer "user_id"
