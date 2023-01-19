1e36b1c524fa8cd1478dde15c608952a4d14858c
EC2 Default User <ec2-user@ip-172-31-35-105.ap-northeast-1.compute.internal>
2023-01-19 08:57:47 +0000

    [add] コメント/いいね機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   
-  resources :users, only: [:show, :edit ,:update]
-  
-  devise_for :users ,path: 'user'
+  get "/homes/about" => "homes#about", as: "about"
+  devise_for :users
   root to: 'homes#top'
   
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
-  get "/homes/about" => "homes#about", as: "about"
+  resources :users, only: [:show, :edit ,:update]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
